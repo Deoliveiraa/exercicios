f161ca0f86ad3b2a70af85f99a30331cb5255924
Deoliveiraa <lucas187_ro@hotmail.com>
2022-08-06 17:40:38 -0300

    Primeiro exercicio

---
+++ exercicio01.py
@@ -0,0 +1,28 @@
+opcao = int(input("Qual tabuada vc quer ver, digite o primeiro numero da tabuada que deseja ver: "))
+if opcao == 2:
+    for x in range(2 , 22 , 2):
+     print(x)
+elif opcao == 3:
+    for a in range(3 , 33 , 3):
+     print(a)
+elif opcao == 4:
+    for a in range(4 , 44 , 4):
+     print(a)
+elif opcao == 5:
+    for a in range(5 , 55 , 5):
+     print(a)
+elif opcao == 6:
+    for a in range(6 , 66 , 6):
+     print(a)
+elif opcao == 7:
+    for a in range(7 , 77 , 7):
+     print(a)
+elif opcao == 8:
+    for a in range(8 , 88 , 8):
+     print(a)
+elif opcao == 9:
+    for a in range(9 , 99 , 9):
+     print(a)
+elif opcao == 10:
+    for a in range(10 , 100 , 10):
+     print(a)
