6e9d314034aa8f123e42a484c54c3739dc723c87
Deoliveiraa <lucas187_ro@hotmail.com>
2022-11-11 16:36:21 -0300

    trabalho

--- exercicio17.py
+++ exercicio17.py
@@ -8,23 +8,27 @@ def calculo():
     global subtotal
     print('Vamos Caluclar a media das suas notas')
     while True:
-        nota = float(input('Entre com o valor da nota tirado: '))
-        porcentagem = float(input('Entre com o valor da porcentagem da nota: '))
-        resultado = (porcentagem / 100 ) * nota 
-        subtotal += resultado 
-        sair = input('Deseja Calcular mais alguma nota ?')
-        if (sair.lower() == 'sim') or (sair.lower() == 's'):
-            continue
-        elif (sair.lower() == 'nao') or (sair.lower() == 'n'):
-            return
-        else:
-            print('Apenas sim para continuar ou não para sair')
-            sair = input('Deseja Calcular mais alguma nota ?')
+        try:   
+            nota = float(input('Entre com o valor da nota tirado: '))
+            porcentagem = float(input('Entre com o valor da porcentagem da nota: '))
+            resultado = (porcentagem / 100 ) * nota 
+            subtotal += resultado 
+            sair = input('Deseja Calcular mais alguma nota ?') 
             if (sair.lower() == 'sim') or (sair.lower() == 's'):
-              continue
+                continue
             elif (sair.lower() == 'nao') or (sair.lower() == 'n'):
-               return
-
+                return
+            else:
+                print('Apenas sim para continuar ou não para sair')
+                sair = input('Deseja Calcular mais alguma nota ?')
+                if (sair.lower() == 'sim') or (sair.lower() == 's'):
+                    continue
+                elif (sair.lower() == 'nao') or (sair.lower() == 'n'):
+                    return
+        except ValueError:
+            print('!!! Apenas Numeros !!!')
+            continue        
+  
 # Main
 calculo()
 print('a media de suas notas são: {}'.format(subtotal))
