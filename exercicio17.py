e2ad1211eae4a42b1a173210a51218b8811523c4
Deoliveiraa <lucas187_ro@hotmail.com>
2022-11-11 16:27:48 -0300

    exercicio

--- exercicio17.py
+++ exercicio17.py
@@ -6,9 +6,9 @@ subtotal = 0
 
 def calculo():
     global subtotal
+    print('Vamos Caluclar a media das suas notas')
     while True:
-        nota = float(input("Vamos Caluclar a media das suas notas \n"
-                            'Entre com o valor da nota tirado: '))
+        nota = float(input('Entre com o valor da nota tirado: '))
         porcentagem = float(input('Entre com o valor da porcentagem da nota: '))
         resultado = (porcentagem / 100 ) * nota 
         subtotal += resultado 
@@ -19,7 +19,11 @@ def calculo():
             return
         else:
             print('Apenas sim para continuar ou não para sair')
-
+            sair = input('Deseja Calcular mais alguma nota ?')
+            if (sair.lower() == 'sim') or (sair.lower() == 's'):
+              continue
+            elif (sair.lower() == 'nao') or (sair.lower() == 'n'):
+               return
 
 # Main
 calculo()
